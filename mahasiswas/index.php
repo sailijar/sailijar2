abae6acd85d9f2db64c901a0c919badf6ece0163
sailijar <sailijar88@gmail.com>
2024-12-18 15:45:55 +0700

    menhapus folder mahasiswa

--- mahasiswas/index.php
+++ mahasiswas/index.php
@@ -39,7 +39,9 @@
                     include("../koneksi.php");
 
                     #2. menulis query
-                    $tampil = "SELECT * FROM mahasiswas";
+                    $tampil = "SELECT *, mahasiswas.nama as nm_mhs, dosens.nama as nm_dos FROM mahasiswas 
+                    INNER JOIN jurusans ON mahasiswas.jurusans_id=jurusans.id
+                    INNER JOIN dosens ON mahasiswas.dosens_id=dosens.id";
 
                     #3. jalankan query
                     $proses = mysqli_query($koneksi, $tampil);
@@ -51,18 +53,44 @@
                     <tr>
                         <th scope="row"><?=$nomor++?></th>
                         <td><?=$data['nim']?></td>
-                        <td><?=$data['nama']?></td>
+                        <td><?=$data['nm_mhs']?></td>
                         <td><?=$data['jk']?></td>
-                        <td><?=$data['jurusans_id']?></td>
-                        <td><?=$data['dosens_id']?></td>
+                        <td><?=$data['jurusan']?></td>
+                        <td><?=$data['nm_dos']?></td>
                         <td>
+                            <!-- TOMBOL DETAIL -->
+                            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#detail<?=$data['id']?>">
+                            <i class="fa-solid fa-eye"></i>
+                            </button>
+
+                            <!-- MODAL DETAIL-->
+                            <div class="modal fade" id="detail<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Data <?=$data['nm_mhs']?> </h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Yakin data <b><?=$data['nama ']?></b> ingin dihapus?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">BTutup</button>
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <!-- TOMBOL EDIT -->
                             <a class="btn btn-info btn-sm" href="edit.php?id=<?=$data['id']?>"><i class="fa fa-pen-to-square"></i></a>
                             
+                            <!-- TOMBOL HAPUS -->
                             <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus<?=$data['id']?>">
                             <i class="fa-solid fa-trash"></i>
                             </button>
 
-                            <!-- Modal -->
+                            <!-- MODAL HAPUS-->
                             <div class="modal fade" id="hapus<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
--- mahasiswas/proses.php
+++ mahasiswas/proses.php
@@ -13,14 +13,18 @@ $jk = $_POST['jk'];
 $jur = $_POST['jur'];
 $dos = $_POST['dos'];
 
+$nama_foto = $_FILES['foto']['name'];
+$tmp_foto = $_FILES['foto']['tmp_name'];
+
 #3. menulis query
-$simpan = "INSERT INTO mahasiswas (nim,nama,tmp_lahir,tgl_lahir,alamat,email,jk,jurusans_id,dosens_id) 
-VALUES ('$nim','$nama','$tempat','$tanggal','$alamat','$email','$jk','$jur','$dos')";
+$simpan = "INSERT INTO mahasiswas (nim,nama,tmp_lahir,tgl_lahir,alamat,email,jk,jurusans_id,dosens_id,foto) 
+VALUES ('$nim','$nama','$tempat','$tanggal','$alamat','$email','$jk','$jur','$dos','$nama_foto')";
 
 #4. jalankan query
 $proses = mysqli_query($koneksi, $simpan);
 
-
+#4.1. Proses Upload File
+$upload_foto = move_uploaded_file($tmp_foto,"foto/$nama_foto");
 
 #5. mengalihkan halaman
 // header("location:index.php");
